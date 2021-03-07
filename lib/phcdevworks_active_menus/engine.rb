f76456c346c065109b011616443e61273c45399c
BradPotts <brad.potts@phcnet.co>
2021-03-07 06:38:33 +0000

    final ajustments before gem release.

--- lib/phcdevworks_active_menus/engine.rb
+++ lib/phcdevworks_active_menus/engine.rb
@@ -1,4 +1,12 @@
 module PhcdevworksActiveMenus
   class Engine < ::Rails::Engine
+
+    # Theme Dependencies
+    require "phcthemes_admin_panel_pack"
+    require "phcthemes_web_theme_pack"
+
+    # Plugin Namespace
+    isolate_namespace PhcdevworksActiveMenus
+
   end
 end
--- lib/phcdevworks_active_menus/version.rb
+++ lib/phcdevworks_active_menus/version.rb
@@ -1,3 +1,3 @@
 module PhcdevworksActiveMenus
-  VERSION = '0.1.0'
+  VERSION = '2.2.2'
 end
